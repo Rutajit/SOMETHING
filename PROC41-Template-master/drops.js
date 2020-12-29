95d48f881a66acb6dbe0870f994880c083f334ea
Rutajit <70809742+Rutajit@users.noreply.github.com>
2020-12-29 19:12:31 +0530

    Add files via upload

---
+++ PROC41-Template-master/drops.js
@@ -0,0 +1,23 @@
+class Createdrop{
+    constuctor(x,y){
+        var options = {
+            friction:0.001,
+            restituion:0.1
+        }
+
+        this.rain =Bodies.circle(x,y,50,options)
+this.radius = 5
+
+World.add(world,this.rain)
+    }
+    updateY(){
+        if(this.rain.position.y>height){
+ Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
+        }
+    }
+    showdrop(){
+    fill("blue")
+    ellipseMode(CENTER)
+    ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius)
+    }
+}
